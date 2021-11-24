99a49d3384310af903b4a0e0390e6551cb020a50
unix77 <imunix7@gmail.com>
2021-11-24 13:53:53 -0300

    Finished the first exercise

--- js/first_practice_script.js
+++ js/first_practice_script.js
@@ -23,3 +23,34 @@
 
 'use strict';
 
+let lastMovieA = '',
+    ratingA = 0,
+    lastMovieB = '',
+    ratingB = 0;
+    
+    
+
+//1
+// "+" transforms the string from prompt, to a nunber
+let numberOfFilms = +prompt("How many movies have you seen ?", 'Insert number');
+
+//2
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {}, //empty object
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+//3
+lastMovieA = prompt("Tell me one of the last movies you have seen ?", 'Insert movie');
+ratingA = prompt("From 0 to 10, how much did you like it ?", 'Insert numebr');
+
+lastMovieB = prompt("Tell me one of the last movies you have seen ?", 'Insert movie');
+ratingB = prompt("From 0 to 10, how much did you like it ?", 'Insert numebr');
+
+personalMovieDB.movies[lastMovieA] = ratingA;
+personalMovieDB.movies[lastMovieB] = ratingB;
+
+console.log(personalMovieDB);

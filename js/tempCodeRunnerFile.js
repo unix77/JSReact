a627fe789fe9cde5e2ab15d58efc2212d5c0ab60
unix77 <imunix7@gmail.com>
2021-11-28 12:59:23 -0300

    Fixing stuff

--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,17 +1,1 @@
-//this is an object
-const options = {
-	name: 'test',
-	width: 1024,
-	colors: {
-		border: 'black',
-		bg: 'red'
-	}
-};
-
-//iterate the attributes of an object, object[key] gives us the value of the attribute~key
-for(let key in options) {
-	console.log(`Attribute ${key} whose value is ${options[key]}`);
-}
-
-close.log(Object.keys(options));
-
+console.log("hola");

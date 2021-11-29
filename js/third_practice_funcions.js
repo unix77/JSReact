ae9db6f5ed001e9eddac9dcfbbc562f689606b9f
unix77 <d.shkolyar7@gmail.com>
2021-11-29 00:54:12 -0300

    Added the third practical exercise

--- js/third_practice_funcions.js
+++ js/third_practice_funcions.js
@@ -15,4 +15,76 @@ P.S. Функции вызывать не обязательно*/
 
 // Код возьмите из предыдущего домашнего задания
 
-console.log("hola");
+
+
+let lastMovie = ' ',
+    rating = 0,
+    i = 0,
+    numberOfFilms = 0;
+
+function start(){
+    numberOfFilms = +prompt("How many movies have you seen ?", 'Insert number');
+
+    while( numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt("How many movies have you seen ?", 'Insert number');
+    }
+}
+
+start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {}, //empty object
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+function rememberMyFilms(){
+    while( i < numberOfFilms ){
+        lastMovie = prompt("Tell me one of the last movies you saw", '');
+        rating = prompt("From 0 to 10, how much did you like it ?", 'Insert number');
+        if(lastMovie.length > 50 || lastMovie == '' || 0 > rating > 10){
+            console.log("Try again !");
+        }
+        else{
+            personalMovieDB.movies[lastMovie] = rating;
+            i++;
+        }
+    }
+}
+
+rememberMyFilms();
+
+function detectLvl(){
+    let aux = personalMovieDB.count;
+    console.log(aux);
+    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa");
+    if(aux < 3){
+        console.log("Junior cineman");
+    }
+    else if(5 > aux > 3){
+        console.log("Medium cineman");
+    }
+    else if(8 > aux > 5){
+        console.log("Expert cineman");
+    }    
+    
+}
+
+detectLvl();
+
+function showMyDB(){
+    if(!personalMovieDB.privat){
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
+
+function writeGenres(){
+    for(let i = 0; i < 2 ; i++){
+        personalMovieDB.genres[i] = prompt(`Your favourite genre N ${i+1}`);
+    }
+}
+
